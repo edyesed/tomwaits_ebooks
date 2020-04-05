617237561d0dea93223b91b9c57506472699d551
Ed Anderson <ed.anderson@outreach.io>
2020-04-04 20:49:52 -0700

    what chump added that second 'traverse the subtree' bit?! that was fucking everything up

--- sentence_chunker_possessives.py
+++ sentence_chunker_possessives.py
@@ -9,6 +9,7 @@ from neo4j import GraphDatabase
 
 # uri = "bolt://localhost:7687"
 uri = os.environ.get('NEO_URL', "bolt://neo:7687")
+DEBUG = os.environ.get('DEBUG', False)
 
 WORKING_DIR='./songs_pos_tagged_pickles'
 ## The pickles in INPUT_DIR are pickled whole song texts with tokenized words.
@@ -27,13 +28,16 @@ INPUT_DIR='./pickled_lyrics'
 #"""
 
 GRAMMAR = r"""
-  NP: {<CD|DT|PP\$>?<JJ[RS]*>*<NN.*>+} # Chunk sequences of DT, JJ, NN
-  PP: {<IN><NP>}                  # Chunk prepositions followed by NP
-  VPastPart: {<VBN><IN>}          # Chunk verbs and their arguments
-  VP: {<RB>?<VB[^RZ].*>}                # Chunk verbs and their arguments
-  NOUNANDVERB: {<NP><VP>}         # Chunk NP to VP
+  NP: {<CD|DT|PP\$>?<JJ[RS]*|PRP\$*>*<NN.*>+} # Chunk sequences of DT, JJ, NN, chink out VPs
+  PP: {<IN><NP>}                           # Chunk prepositions followed by NP
+  VP: {<RB>?<VB[^RZ].*>+(<PRP><JJ>)*}             # Chunk verbs and their arguments
+  INTERROGATION: {<VB.*><PRP><VB.*>}             # Ask a question
+  INTERROGATIONLIKE: {<INTERROGATION><IN><PRP><.*>+?<NP>}   # Ask a question like a something
+  VPastPart: {<VBN><IN>}                   # Chunk verbs and their arguments
+  NOUNANDVERB: {<NP><VP>}                  # Chunk NP to VP
   VERBANDPREP: {<VP><PP>}
   """
+
 
 def list_files(dir=""):
     files = [ f for f in os.listdir(dir) if 'pkl' in f ]
@@ -49,7 +53,6 @@ if __name__ == "__main__":
     for i in range(1,60):
         while True:
             try:
-                # driver = GraphDatabase.driver(uri, auth=("neo4j", "bitnami"), connection_timeout=120)
                 driver = GraphDatabase.driver(uri, connection_timeout=120)
             except Exception as e:
                 print(f"Error number {i}. {e}.")
@@ -75,11 +78,15 @@ if __name__ == "__main__":
                     lyric_flat = ' '.join([w[0] for w in sentence])
 
                     chunks = cp.parse(sentence)
-                    # care_about = ['NP', 'VP', 'PP', 'VPastPart', 'VERBANDPREP' ]
-                    care_about = ['NP', 'VERBANDPREP', 'NOUNANDVERB']
+                    if DEBUG:
+                        print(chunks.pprint())
+                    care_about = ['VP', 'NP', 'VERBANDPREP', 'INTERROGATIONLIKE', 'INTERROGATION' ]
                     for chunk_idx, chunk in enumerate(chunks.subtrees(filter=lambda t: t.label() in care_about )):
-                        # We ensure that we send up the lyric once so we can relationship to it
-                        # and the phrase 
+                        if DEBUG:
+                            print(chunk)
+                            print("")
+                        # We ensure that we send up the lyric once so we can relationship the lyric
+                        #    to the phrase 
                         cypher = f"""
                                     MERGE (l:Lyric {{ text: "{lyric_flat}" }}) 
                                 """
@@ -89,14 +96,9 @@ if __name__ == "__main__":
                                  MERGE (pl{chunk_idx}:Phrase {{ text: "{phrase}" }})
                                  MERGE (pl{chunk_idx})<-[rl{chunk_idx}:{chunk.label()}]-(l)
                              """
-                        for counter, subt in enumerate(chunk.subtrees()):
-                            phrase = ' '.join([x[0] for x in subt.flatten()]).lower()
-                            cypher += f"""
-                                MERGE (p{counter}:Phrase {{ text: "{phrase}" }})
-                                MERGE (p{counter})<-[r{counter}:{chunk.label()}]-(l)
-                            """
                         try:
-                            #print(cypher)
+                            if DEBUG:
+                                print(cypher)
                             session.run(cypher)
                         except Exception as e:
                             print(f"EXCEPTION RAISED phrase: {phrase}")
